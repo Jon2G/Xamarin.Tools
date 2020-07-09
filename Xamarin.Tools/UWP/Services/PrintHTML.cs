697a4cf5a0771a2c9a74c0012c4095075a5a98e4
Jonathan <blum.desarrollo.jonathang@gmail.com>
2020-07-09 10:11:15 -0500

    Convertir Html en pdf

--- Xamarin.Tools/UWP/Services/PrintHTML.cs
+++ Xamarin.Tools/UWP/Services/PrintHTML.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using PdfSharp;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing.Printing;
 using System.IO;
 using System.Text;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
 using Xamarin.Forms;
 
 namespace Plugin.Xamarin.Tools.UWP.Services
@@ -13,7 +17,7 @@ namespace Plugin.Xamarin.Tools.UWP.Services
         {
             try
             {
-                DirectoryInfo TicketsPath = new DirectoryInfo(Shared.Tools.Instance.LibraryPath+ "\\TICKETS");
+                DirectoryInfo TicketsPath = new DirectoryInfo(Shared.Tools.Instance.LibraryPath + "\\TICKETS");
                 if (!TicketsPath.Exists)
                 {
                     TicketsPath.Create();
@@ -23,36 +27,109 @@ namespace Plugin.Xamarin.Tools.UWP.Services
                 {
                     return false;
                 }
-                FileInfo file = new FileInfo(TicketsPath.FullName+$"\\{Guid.NewGuid().ToString("N")}.html");
-                File.WriteAllText(file.FullName, HTML);
-                file.Refresh();
-                if (file.Exists)
+                string TicketPath = $"{TicketsPath.FullName}\\{Guid.NewGuid():N}";
+                if (Printer == "Microsoft Print to PDF")
                 {
-                    if (Printer == "Microsoft Print to PDF")
+                    FileInfo file = new FileInfo($"{TicketPath}.html");
+                    File.WriteAllText(file.FullName, HTML);
+                    file.Refresh();
+                    if (file.Exists)
                     {
                         Process.Start(file.FullName);
                         return true;
                     }
-                    // Spawn the code to print the packing slips
-                    ProcessStartInfo info = new ProcessStartInfo();
-                    info.Arguments = $"-p \"{Printer}\" \"{file.FullName}\"";
-                    string pathToExe = Shared.Tools.Instance.LibraryPath + "\\PrintHTML";
-                    info.FileName = pathToExe+ "\\PrintHtml.exe";
-                    using (Process p = Process.Start(info))
-                    {
-                        // Wait until it is finished
-                        while (!p.HasExited)
-                        {
-                            System.Threading.Thread.Sleep(10);
-                        }
-                        // Return the exit code
-                        return p.ExitCode == 0;
-                    }
+                }
+                else
+                {
+                    return PrintToPDF(HTML, TicketPath, Printer);
                 }
             }
-            catch
+            catch(Exception ex)
             {
+                SQLHelper.Log.LogMe(ex, "Al guardar el archivo html");
                 return false;
+            }
+            return false;
+        }
+        private bool PrintToPDF(string HTML, string TicketPath, string Printer)
+        {
+            try
+            {
+                FileInfo pdfile = new FileInfo($"{TicketPath}.pdf");
+                PdfGenerateConfig config = new PdfGenerateConfig();
+                config.ManualPageSize = new PdfSharp.Drawing.XSize(226,int.MaxValue);
+                config.MarginBottom =
+                    config.MarginTop =
+                    config.MarginLeft =
+                    config.MarginRight = 0;
+
+                using (PdfDocument pdf = PdfGenerator.GeneratePdf(HTML, config))
+                {
+                    pdf.Save(pdfile.FullName);
+                }
+                if (pdfile.Exists)
+                {
+                    return PrintPDF(Printer, "80mm", pdfile.FullName, 1);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                SQLHelper.Log.LogMe(ex, "Al convertir un ticket html a pdf");
+            }
+            return false;
+        }
+        private bool PrintPDF(string printer,string paperName,string filename,int copies)
+        {
+            try
+            {
+                // Create the printer settings for our printer
+                var printerSettings = new PrinterSettings
+                {
+                    PrinterName = printer,
+                    Copies = (short)copies,
+                };
+
+                // Create our page settings for the paper size selected
+                var pageSettings = new PageSettings(printerSettings)
+                {
+                    Margins = new Margins(0, 0, 0, 0),
+                };
+                if (paperName == "58mm")
+                {
+                    pageSettings.PaperSize = new PaperSize("TK", 2, int.MaxValue);
+                }
+                else if (paperName == "80mm")
+                {
+                    pageSettings.PaperSize = new PaperSize("TK", 3, int.MaxValue);
+                }
+                else
+                {
+                    foreach (PaperSize paperSize in printerSettings.PaperSizes)
+                    {
+                        if (paperSize.PaperName == paperName)
+                        {
+                            pageSettings.PaperSize = paperSize;
+                            break;
+                        }
+                    }
+                }
+                // Now print the PDF document
+                using (var document = PdfiumViewer.PdfDocument.Load(filename))
+                {
+                    using (var printDocument = document.CreatePrintDocument())
+                    {
+                        printDocument.PrinterSettings = printerSettings;
+                        printDocument.DefaultPageSettings = pageSettings;
+                        printDocument.PrintController = new StandardPrintController();
+                        printDocument.Print();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                SQLHelper.Log.LogMe(ex, "Al imprimir un pdf");
             }
             return false;
         }
