d189bf742031347ad0462e5cf6c43ca99ce0405a
EduardoBlum <eduardoblumitech@gmail.com>
2020-10-08 10:07:03 -0500

    bug fixes

--- SQLHelper/SQLH.cs
+++ SQLHelper/SQLH.cs
@@ -40,13 +40,11 @@ namespace SQLHelper
             }
             return null;
         }
-
         public bool TriggerExists(string TriggerName)
         {
             return Exists($"SELECT * FROM sys.objects WHERE [name] = N'{TriggerName}' AND [type] = 'TR'"
                 , false);
         }
-
         public List<Tuple<string, Type>> GetColumnsName(string consulta, params SqlParameter[] Parametros)
         {
             List<Tuple<string, Type>> listacolumnas = new List<Tuple<string, Type>>();
