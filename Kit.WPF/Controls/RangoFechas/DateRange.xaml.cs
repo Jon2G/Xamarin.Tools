031ea946e16222f67e53d1febf02596fd48ee141
Jonathan <blum.desarrollo.jonathang@gmail.com>
2020-11-23 14:07:00 -0600

    DateRange

--- Kit.WPF/Controls/RangoFechas/DateRange.xaml.cs
+++ Kit.WPF/Controls/RangoFechas/DateRange.xaml.cs
@@ -18,12 +18,36 @@ namespace Kit.WPF.Controls.RangoFechas
     /// <summary>
     /// Lógica de interacción para DateRange.xaml
     /// </summary>
-    public partial class DateRange : UserControl
+    public partial class DateRange : ObservableUserControl
     {
+        #region Buscador
+        private Rango _Rango;
+        public Rango Rango
+        {
+            get => _Rango;
+            set
+            {
+                _Rango = value;
+                OnPropertyChanged();
+            }
+        }
+        public static readonly DependencyProperty RangoProperty =
+            DependencyProperty.Register(
+                nameof(DateRange.Rango), typeof(Rango), typeof(DateRange),
+                new FrameworkPropertyMetadata(
+                    null,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+        #endregion
+
+        public Rango Fechas { get => (Rango)Rango; }
         public DateRange()
         {
             this.DataContext = this;
+            this.Rango = new ModeloRango();
             InitializeComponent();
+
         }
     }
 }
