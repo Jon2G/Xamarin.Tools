5335d1ffab46beb9c9b0ccc1371d76bd6bb75ef3
Jonathan <blum.desarrollo.jonathang@gmail.com>
2021-01-22 14:04:56 -0600

    Observable Raise on controls

--- Kit.WPF/Controls/ObservableWindow.cs
+++ Kit.WPF/Controls/ObservableWindow.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using Kit.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Expression = System.Linq.Expressions.Expression;
 
 namespace Kit.WPF.Controls
 {
-    public class ObservableWindow : Window, INotifyPropertyChanged
+    public class ObservableWindow : Window, KitINotifyPropertyChanged
     {
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
@@ -23,6 +26,35 @@ namespace Kit.WPF.Controls
         {
             var handler = PropertyChanged;
             handler?.Invoke(this, args);
+        }
+        #endregion
+
+        #region PerfomanceHelpers
+        public void Raise<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (this.PropertyChanged != null)
+            {
+                var body = propertyExpression.Body as MemberExpression;
+                if (body == null)
+                    throw new ArgumentException("'propertyExpression' should be a member expression");
+
+                var expression = body.Expression as ConstantExpression;
+                if (expression == null)
+                    throw new ArgumentException("'propertyExpression' body should be a constant expression");
+
+                object target = Expression.Lambda(expression).Compile().DynamicInvoke();
+
+                var e = new PropertyChangedEventArgs(body.Member.Name);
+                PropertyChanged(target, e);
+            }
+        }
+
+        public void Raise<T>(params Expression<Func<T>>[] propertyExpressions)
+        {
+            foreach (var propertyExpression in propertyExpressions)
+            {
+                Raise<T>(propertyExpression);
+            }
         }
         #endregion
     }
