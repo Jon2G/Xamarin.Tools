44cc020187f478c210442ff2e9d316a9f841884d
Jonathan <blum.desarrollo.jonathang@gmail.com>
2021-04-05 14:21:05 -0500

    Elevator for class

---
+++ Kit/Extensions/Elevator.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace Kit.Extensions
+{
+    public static class Elevator
+    {
+        public static T Elevate<T>(this object Lower)
+            where T : new()
+        {
+            var ElevatedType = typeof(T);
+            var LowerType = Lower.GetType();
+            if (LowerType.IsSubclassOf(ElevatedType))
+            {
+                throw new InvalidCastException($"{LowerType.Name} is not subclass of {ElevatedType.Name}");
+            }
+            string json = JsonSerializer.Serialize(Lower);
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
