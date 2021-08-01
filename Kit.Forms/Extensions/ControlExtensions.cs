7700d0c69cb3df6aaca994ea5fc1d4c9da5a35e3
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2021-07-31 21:26:42 -0500

    Se agrego ControlExtensions

---
+++ Kit.Forms/Extensions/ControlExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Kit
+{
+    public static class ControlExtensions
+    {
+        public static T FindParent<T>(this Xamarin.Forms.Element element)
+        {
+            Element parent = element;
+            while (parent != null)
+            {
+                parent = parent.Parent;
+                if(parent is T found)
+                {
+                    return found;
+                }
+            }
+            return default(T);
+        }
+    }
+}
