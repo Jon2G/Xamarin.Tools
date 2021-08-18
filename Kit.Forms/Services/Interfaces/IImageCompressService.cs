5ea9c1dae30299a4b6fb29fde9dcf05bf30943bd
Jon2G <jonyjouna@gmail.com>
2021-08-17 20:02:18 -0500

    Compress Image

---
+++ Kit.Forms/Services/Interfaces/IImageCompressService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kit.Forms.Services.Interfaces
+{
+    public interface IImageCompressService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="imageData"></param>
+        /// <param name="Quality">From 0 to 100</param>
+        /// <returns></returns>
+        public Task<FileStream> CompressImage(Stream imageData, int Quality);
+    }
+}
---
+++ Kit.iOS/Services/ImageCompressService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FFImageLoading.Extensions;
+using Foundation;
+using Kit.iOS.Services;
+using Kit.Forms.Services.Interfaces;
+using UIKit;
+using Xamarin.Forms;
+[assembly: Dependency(typeof(ImageCompressService))]
+
+namespace Kit.iOS.Services
+{
+    public class ImageCompressService : IImageCompressService
+    {
+        public async Task<FileStream> CompressImage(Stream imageData, int Quality)
+        {
+            await Task.Yield();
+            string path = System.IO.Path.Combine(Kit.Tools.Instance.TemporalPath, $"{Guid.NewGuid():N}.jpeg");
+            using (NSData nsData = NSData.FromStream(imageData))
+            {
+                using (UIImage uiimage = UIImage.LoadFromData(nsData))
+                {
+                    using (Stream compressed = uiimage.AsJpegStream(Quality))
+                    {
+                        FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+                        await compressed.CopyToAsync(stream);
+                        return stream;
+                    }
+                }
+            }
+        }
+    }
+}
