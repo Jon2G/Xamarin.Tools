570b86957428aebd3445b40e9a180e91a6fb98f3
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2021-09-05 23:41:38 -0500

    ValidationModel

---
+++ Kit.Forms/Validations/INotifyScrollToProperty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kit.Forms.Validations
+{
+    public interface INotifyScrollToProperty
+    {
+        event ScrollToPropertyHandler ScrollToProperty;
+    }
+
+    public delegate void ScrollToPropertyHandler(string PropertyName);
+}
