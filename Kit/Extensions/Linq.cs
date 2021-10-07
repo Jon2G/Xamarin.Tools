b245a99138267fc84174628836c5787d0d9800a4
Jon2G <jonyjouna@gmail.com>
2021-10-07 10:00:01 -0500

    Contains Any

--- Kit/Extensions/Linq.cs
+++ Kit/Extensions/Linq.cs
@@ -309,5 +309,123 @@ namespace Kit
 
             return left;
         }
+        public static TSource Last<TSource>(this IEnumerable<TSource> source)
+        {
+            TSource last = source.TryGetLast(out bool found);
+            if (!found)
+            {
+                throw  new Exception("No elements!");
+            }
+
+            return last;
+        }
+
+        public static TSource Last<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            TSource last = source.TryGetLast(predicate, out bool found);
+            if (!found)
+            {
+                throw new Exception("No match");
+            }
+
+            return last;
+        }
+
+        public static TSource LastOrDefault<TSource>(this IEnumerable<TSource> source) =>
+            source.TryGetLast(out bool _);
+
+        public static TSource LastOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) =>
+            source.TryGetLast(predicate, out bool _);
+
+        private static TSource TryGetLast<TSource>(this IEnumerable<TSource> source, out bool found)
+        {
+            if (source == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (source is IList<TSource> list)
+            {
+                int count = list.Count;
+                if (count > 0)
+                {
+                    found = true;
+                    return list[count - 1];
+                }
+            }
+            else
+            {
+                using (IEnumerator<TSource> e = source.GetEnumerator())
+                {
+                    if (e.MoveNext())
+                    {
+                        TSource result;
+                        do
+                        {
+                            result = e.Current;
+                        }
+                        while (e.MoveNext());
+
+                        found = true;
+                        return result;
+                    }
+                }
+            }
+
+            found = false;
+            return default(TSource);
+        }
+
+        private static TSource TryGetLast<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, out bool found)
+        {
+            if (source == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (predicate == null)
+            {
+                throw new NullReferenceException();
+            }
+            if (source is IList<TSource> list)
+            {
+                for (int i = list.Count - 1; i >= 0; --i)
+                {
+                    TSource result = list[i];
+                    if (predicate(result))
+                    {
+                        found = true;
+                        return result;
+                    }
+                }
+            }
+            else
+            {
+                using (IEnumerator<TSource> e = source.GetEnumerator())
+                {
+                    while (e.MoveNext())
+                    {
+                        TSource result = e.Current;
+                        if (predicate(result))
+                        {
+                            while (e.MoveNext())
+                            {
+                                TSource element = e.Current;
+                                if (predicate(element))
+                                {
+                                    result = element;
+                                }
+                            }
+
+                            found = true;
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            found = false;
+            return default(TSource);
+        }
     }
 }
