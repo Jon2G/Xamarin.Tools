97b151ff56693455425f18fa30f514cc6f3d706a
Jon2G <jonyjouna@gmail.com>
2021-11-30 15:13:03 -0600

    Decimal count extension

---
+++ Kit/Extensions/DecimalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kit
+{
+    public static class DecimalExtensions
+    {
+        public static int CountDecimalDigits(this decimal n)
+        {
+            return n.ToString(System.Globalization.CultureInfo.InvariantCulture)
+                    //.TrimEnd('0') uncomment if you don't want to count trailing zeroes
+                    .SkipWhile(c => c != '.')
+                    .Skip(1)
+                    .Count();
+        }
+    }
+}
