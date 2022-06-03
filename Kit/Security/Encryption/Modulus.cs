b2db4b51404bb9de7592554972eae39e9c1f1c37
Jon2G <jonyjouna@gmail.com>
2022-06-03 14:05:20 -0500

    Update Modulus.cs

--- Kit/Security/Encryption/Modulus.cs
+++ Kit/Security/Encryption/Modulus.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Kit.Security.Encryption
 {
     public static class Modulus
     {
+        public static BigInteger Mod(BigInteger ba, BigInteger bb)
+        {
+            return ((ba * bb) + ba) % bb;
+        }
+        public static ulong Mod(ulong a, ulong b)
+        {
+            return ((a * b) + a) % b;
+        }
         public static int Mod(int a, int b)
         {
             return (Math.Abs(a * b) + a) % b;
