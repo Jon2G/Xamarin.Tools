99ea9cc085d7df2e4b84cc135862dd50cd1cbe78
Jon2G <jonyjouna@gmail.com>
2022-06-03 02:14:49 -0500

    Create Modulus.cs

---
+++ Kit/Security/Encryption/Modulus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kit.Security.Encryption
+{
+    public static class Modulus
+    {
+        public static int Mod(int a, int b)
+        {
+            return (Math.Abs(a * b) + a) % b;
+        }
+    }
+}
