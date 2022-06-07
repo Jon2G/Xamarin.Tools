8feec5d0f81be8d3009992ca8a5f2e0640d20976
Jon2G <jonyjouna@gmail.com>
2022-06-07 11:52:52 -0500

    Create ConsoleColor.cs

---
+++ Kit/Extensions/ConsoleColor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Kit
+{
+    public static class ConsoleColor
+    {
+        public static string ToHex(this System.ConsoleColor color)
+        {
+            switch (color)
+            {
+                case System.ConsoleColor.White:
+                    return "#FFFFFF";
+
+                case System.ConsoleColor.Black:
+                    return "#000000";
+
+                case System.ConsoleColor.DarkMagenta:
+                    return "#702963";
+                case System.ConsoleColor.Blue:
+                    return "#0000FF";
+                case System.ConsoleColor.DarkBlue:
+                    return "#000099";
+                case System.ConsoleColor.Cyan:
+                    return "#00FFFF";
+                case System.ConsoleColor.DarkGreen:
+                    return "#006400";
+                case System.ConsoleColor.Green:
+                    return "#00FF00";
+                case System.ConsoleColor.Gray:
+                    return "#808080";
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
